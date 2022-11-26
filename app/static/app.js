77a1831716c48960c3d0137bc164043b5093223e
bdiazc90 <bdiazc90@gmail.com>
2022-11-26 13:18:26 -0500

    Fetch (PUT) update

--- app/static/app.js
+++ app/static/app.js
@@ -1,27 +1,36 @@
 console.log("Hola a todos, soy Javascript");
 
+const taskId = document.querySelector("#taskId").innerText;
+const taskSection = document.querySelector("#task_section");
+const editButton = taskSection.querySelector("a.btn");
+
 function edit() {
 	const spanText = document.querySelector("#taskText");
 	const taskText = spanText.innerText;
 
 	const inputText = `<input type="text" id="inputText" class="form-control d-inline" placeholder="${taskText}" value="${taskText}">`;
 	const btnSave = `<a href="javascript: save();" class="btn btn-primary">guardar</a>`;
-	spanText.innerHTML += inputText + btnSave;
-
-	console.log("editando...");
+	taskSection.innerHTML = inputText + btnSave;
 }
 
 function save() {
-	const id = document.querySelector("#taskId").innerText;
 	const text = document.querySelector("#inputText").value;
 
 	const formData = new FormData();
 	formData.append("text", text);
 	// XHR : PUT
-	fetch("/update/" + id, {
+	fetch("/update/" + taskId, {
 		method: "PUT",
 		body: formData,
 	})
 		.then((response) => response.json())
-		.then((data) => console.log(data));
+		.then(
+			(data) =>
+				(taskSection.innerHTML = `
+		<h1 class="display-4 mt-3">
+		<span id="taskId">${data.id}</span>:
+		<span id="taskText">${data.text}</span>
+	</h1>
+	<a class="btn btn-secondary" href="javascript: edit(this);">editar</a>`)
+		);
 }
